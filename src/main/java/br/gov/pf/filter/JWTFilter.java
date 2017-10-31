bc25ec9f3bc5e468cfc94b302620da47996e0fe8
Lucas Sartori <faltou.criatividade0@gmail.com>
2017-10-30 23:28:28 -0200

    adicionado filtro do token -- ainda nao concluido a autenticacao

---
+++ src/main/java/br/gov/pf/filter/JWTFilter.java
@@ -0,0 +1,50 @@
+package br.gov.pf.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by sartori on 30/10/2017
+ */
+public class JWTFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+
+        final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
+        final String authHeader = request.getHeader("authorization");
+
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(request, response);
+        } else {
+
+            if (authHeader == null || !authHeader.startsWith("Bearer "))
+                throw new ServletException("Missing or invalid Authorization header");
+            final String token = authHeader.substring(7);
+
+            try {
+                final Claims claims = Jwts.parser().setSigningKey("secretkey").parseClaimsJws(token).getBody();
+                request.setAttribute("claims", claims);
+            } catch (final SignatureException e) {
+                throw new ServletException("Invalid token.");
+            }
+
+            filterChain.doFilter(request, response);
+        }
+
+    }
+}
