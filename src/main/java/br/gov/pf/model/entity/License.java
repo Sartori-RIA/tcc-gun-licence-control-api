989ca3109d78c291ef2b3de440b112f2f88e0caa
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-04-26 09:11:09 -0300

    ajustes

--- src/main/java/br/gov/pf/model/entity/License.java
+++ src/main/java/br/gov/pf/model/entity/License.java
@@ -32,6 +32,9 @@ public class License extends AbstractEntity {
     @ManyToOne
     private @NotNull User user;
 
+    @ManyToOne
+    private Address address;
+
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @Fetch(FetchMode.SUBSELECT)
     @JoinTable(name = "licence_exam",
